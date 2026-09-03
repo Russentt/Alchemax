80b5fc26620d677cdc3b3689202054b4653c2437
Russentt <ignacio.battistoni.mestre@gmail.com>
2026-09-02 22:50:04 -0400

    Agregar formulario y ventana modal

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const formulario = document.querySelector(".formulario");
+const modal = document.querySelector(".modal");
+const span = document.querySelector(".close");
 
+formulario.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.style.display = "block";
+});
+
+span.onclick = function () {
+  modal.style.display = "none";
+};

522cc3d1034f96d7a5dd333b4d2444b89213c6b1
Russentt <ignacio.battistoni.mestre@gmail.com>
2026-09-03 20:06:47 -0400

    Traspasar a bootstrap 5

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ formulario.addEventListener("submit", (event) => {
 span.onclick = function () {
   modal.style.display = "none";
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+
+  document.addEventListener("mousemove", (e) => {
+    if (e.clientY <= 80 || header.contains(e.target)) {
+      header.classList.add("nav-visible");
+    } else {
+      header.classList.remove("nav-visible");
+    }
+  });
+
+  let lastScrollY = window.scrollY;
+  window.addEventListener("scroll", () => {
+    if (window.matchMedia("(hover: none)").matches) {
+      if (window.scrollY < lastScrollY) {
+        header.classList.add("nav-visible");
+      } else if (window.scrollY > 80) {
+        header.classList.remove("nav-visible");
+      }
+    }
+    lastScrollY = window.scrollY;
+  });
+});

d4b05c92f5414fbfc5fe03b1898d6a29d5e6fcc2
Lukita-duoc <luc.ortizr@duocuc.cl>
2026-09-06 18:06:24 -0300

    arreglo login

--- assets/js/login.js
+++ assets/js/login.js
@@ -35,22 +35,26 @@ function iniciarSesion() {
         return;
     }
 
-    if (typeof paciente.contrasena === password) {
+    if (typeof paciente.intentos === "undefined") {
         paciente.intentos = 0;
-        localStorage.setItem(clave_pacientes, JSON.stringify(paciente));
+    };
 
-        alert("inicio de session correcto");
+    if (paciente.contrasena === password) {
+        paciente.intentos = 0;
+        localStorage.setItem(clave_pacientes, JSON.stringify(listaPacientes));
+
+        alert("Inicio de sesión correcto");
         sessionStorage.setItem("usuarioActivo", JSON.stringify(paciente));
-        window.location.href = "account.html"
+        window.location.href = "account.html";
     } else {
         paciente.intentos += 1;
 
         if (paciente.intentos >= 3) {
             paciente.bloqueado = true;
-            alert("Cuenta bloqueada, intenta de nuevo mas tarde.");
+            alert("Cuenta bloqueada, intenta de nuevo más tarde.");
         } else {
             const restante = 3 - paciente.intentos;
-            alert("Contraseña/Correo incorrectos");
+            alert(`Contraseña incorrecta. Te quedan ${restante} intento(s).`);
         }
 
         localStorage.setItem(clave_pacientes, JSON.stringify(listaPacientes));

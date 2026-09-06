5609f5bb013e2ccb6c098943306623750715efee
Lukita-duoc <luc.ortizr@duocuc.cl>
2026-09-06 17:13:30 -0300

    Validacion intento login

--- assets/js/login.js
+++ assets/js/login.js
@@ -23,16 +23,38 @@ function iniciarSesion() {
 
     const listaPacientes = JSON.parse(datos);
 
-    const pacEncontrado = listaPacientes.find(paciente => 
-        paciente.correo.toLowerCase() === email && paciente.contrasena === password
-    );
+    const paciente = listaPacientes.find(p => p.correo.toLowerCase() === email);
 
-    if (pacEncontrado) {
-        alert("Inicio de session correcto")
-        sessionStorage.setItem("usuarioActivo", JSON.stringify(pacEncontrado));
-        window.location.href = "account.html";
-    } else {
-        alert("Correo/Clave invalidos")
+    if (!paciente) {
+        alert("correo no registrado");
+        return;
     }
+
+    if (paciente.bloqueado) {
+        alert("Esta cuenta ha sido bloqueada temporalmente");
+        return;
+    }
+
+    if (typeof paciente.contrasena === password) {
+        paciente.intentos = 0;
+        localStorage.setItem(clave_pacientes, JSON.stringify(paciente));
+
+        alert("inicio de session correcto");
+        sessionStorage.setItem("usuarioActivo", JSON.stringify(paciente));
+        window.location.href = "account.html"
+    } else {
+        paciente.intentos += 1;
+
+        if (paciente.intentos >= 3) {
+            paciente.bloqueado = true;
+            alert("Cuenta bloqueada, intenta de nuevo mas tarde.");
+        } else {
+            const restante = 3 - paciente.intentos;
+            alert("Contraseña/Correo incorrectos");
+        }
+
+        localStorage.setItem(clave_pacientes, JSON.stringify(listaPacientes));
+    }
+
 
 };
